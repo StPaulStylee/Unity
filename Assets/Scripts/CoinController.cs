9d3cf34c5b5dfd47f530d394a63ae4d848aa8008
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-02-19 22:19:50 -0600

    Initial Scripts, Materials, Prefabs - Assets

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public float RotationSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Angle of rotation; v = d / t --> d = v * t
+        // Here we are setting the angle for the rotation for this update call by multiplying
+        // our rotation speed by the amount of time that has passed since the last update call
+        // This will ensure that our rotation speeds are constant between all CPUs
+        float angleRotation = RotationSpeed * Time.deltaTime;
+
+        // rotate the coin
+        // The Rotate Method takes a Vector3 and a Scoping param - the scoping
+        // param is defaulted to local but it can be changed to global... This is a GOTCHA
+        // for many newbies so beware that this can happen
+        transform.Rotate(Vector3.up * angleRotation, Space.World);
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Speed of the enemy
+    public float Speed = 3f;
+
+    public float SpeedBoostFactor = 1.2f;
+
+    // Range of movement
+    public float RangeY = 2f;
+
+    private Vector3 initialPosition;
+
+    private int direction = 1;
+
+    void Start()
+    {
+        // Save inital position on start
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // How much are we moving?
+        float movementY = getSpeed( );
+
+        // new position y
+        float currentY= transform.position.y + movementY;
+         
+        // Check whether or not we have left our range
+        // We use absolute value here because our rangeY (range of movement) will always be a positive value
+        // but our object will have negative values that fall beyond the our desired range.
+        if(Mathf.Abs(currentY - initialPosition.y) > RangeY)
+        {
+            direction *= -1;
+        } else
+        {
+            // If we are within our range then we create a new Vector3 and only move it along the X Axis
+            transform.position += new Vector3(0, movementY, 0);
+        }
+    }
+
+    private float getSpeed()
+    {
+        if (direction == 1)
+        {
+            return Speed * Time.deltaTime * direction;
+        } else
+        {
+            return SpeedBoostFactor * Speed * Time.deltaTime * direction;
+        }
+    }
+}
